07cc9d75d2d7b18881d3055c24fcccc8ab0b7d9e
Chamila Gunathilake <chamnilg@gmail.com>
2021-08-27 13:27:52 -0700

    ready

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 export default axios.create({
     baseURL:'https://api.unsplash.com',
     headers:{
-        Authorization:'Client-ID EJ1L_3NBIqI7js-aulacfxUzLLVqW-p6FRa4l2j2TDc'
+        Authorization:KEY
     }  
 })
 
